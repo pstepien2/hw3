62ad6199c63bc1020d495d056ed67d440909ffb6
Patrick Stepien <patrick.stepien@kellogg.northwestern.edu>
2023-05-18 04:54:23 +0000

    hw

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
 
   def new
     @post = Post.new
-    @place = Place.find_by({ "id" => params["company_id"] })
+    @place = Place.find_by({ "id" => params["place_id"] })
     @post["place_id"] = @place["id"]
   end
 
@@ -11,7 +11,7 @@ class PostsController < ApplicationController
     @post["title"] = params["post"]["title"]
     @post["description"] = params["post"]["description"]
     @post["posted_on"] = params["post"]["posted_on"]
-    @post["place_id"] = params["place"]["id"]
+    @post["place_id"] = params["post"]["place_id"]
     @post.save
     redirect_to "/places/#{@post["place_id"]}"
   end
